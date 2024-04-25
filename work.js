0e05658454d33628c6344fa3a3d9e0b3a40041a9
aeter322 <95037990+aeter322@users.noreply.github.com>
2024-04-26 02:09:00 +0300

    Update work.js

--- work.js
+++ work.js
@@ -90,6 +90,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = '${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}';
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
